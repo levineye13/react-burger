873b9bfcbc12590de5302ceb30b78a5e632235bf
Oleg Zharov <levineye@gmail.com>
2022-02-21 20:22:16 +0300

    feat: сверстать компонент карточки бургера

--- src/components/app/app.tsx
+++ src/components/app/app.tsx
@@ -2,11 +2,15 @@ import React from 'react';
 
 import style from './app.module.css';
 import AppHeader from '../app-header/app-header';
+import BurgerIngredients from '../burger-ingredients/burger-ingredients';
 
 function App() {
   return (
     <div className={style.page}>
       <AppHeader />
+      <main>
+        <BurgerIngredients />
+      </main>
     </div>
   );
 }
