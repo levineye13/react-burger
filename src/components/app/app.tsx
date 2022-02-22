2a2c74ed831845b9bdca014bff9cae5775a1b361
Oleg Zharov <levineye@gmail.com>
2022-02-22 18:15:56 +0300

    feat: сверстать конструктор бургера и добавить в app

--- src/components/app/app.tsx
+++ src/components/app/app.tsx
@@ -3,14 +3,16 @@ import React from 'react';
 import styles from './app.module.css';
 import AppHeader from '../app-header/app-header';
 import BurgerIngredients from '../burger-ingredients/burger-ingredients';
+import BurgerConstructor from '../burger-constructor/burger-constructor';
 import data from '../../utils/data';
 
 function App() {
   return (
     <div className={styles.page}>
       <AppHeader />
-      <main>
+      <main className={styles.main}>
         <BurgerIngredients ingredients={data} />
+        <BurgerConstructor ingredients={data} />
       </main>
     </div>
   );
---
+++ src/components/burger-constructor/burger-constructor.tsx
@@ -0,0 +1,88 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import {
+  ConstructorElement,
+  DragIcon,
+  Button,
+  CurrencyIcon,
+} from '@ya.praktikum/react-developer-burger-ui-components';
+
+import styles from './burger-constructor.module.css';
+
+class BurgerConstructor extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      sum: 0,
+    };
+  }
+
+  checkEdgeElement(index, length) {
+    if (index === 0) {
+      return { type: 'top', text: ' (верх)', isLocked: true };
+    }
+
+    if (index === length - 1) {
+      return { type: 'bottom', text: ' (низ)', isLocked: true };
+    }
+
+    return { type: undefined, text: '', isLocked: false };
+  }
+
+  calculateSum() {}
+
+  componentDidMount() {
+    const { ingredients = [] } = this.props;
+
+    if (ingredients.length > 0) {
+      const sum = ingredients.reduce((acc, item) => {
+        return acc + item.price;
+      }, 0);
+
+      this.setState({ sum });
+    }
+  }
+
+  render() {
+    const { ingredients } = this.props;
+
+    return (
+      <section className={`${styles.section} pt-25 pl-4 pr-4 ml-10`}>
+        <ul className={styles.list}>
+          {ingredients.map((item, index) => {
+            const data = this.checkEdgeElement(index, ingredients.length);
+
+            return (
+              <li key={item._id} className={styles.item}>
+                {!data.isLocked && <DragIcon type="primary" />}
+                <ConstructorElement
+                  type={data.type}
+                  thumbnail={item.image}
+                  text={`${item.name} ${data.text}`}
+                  price={item.price}
+                  isLocked={data.isLocked}
+                />
+              </li>
+            );
+          })}
+        </ul>
+        <div className={`${styles.sum} mt-10`}>
+          <span className="text text_type_main-large mr-10">
+            <span className="mr-2">{this.state.sum}</span>
+            <CurrencyIcon type="primary" />
+          </span>
+          <Button type="primary" size="large" htmlType="submit">
+            Оформить заказ
+          </Button>
+        </div>
+      </section>
+    );
+  }
+}
+
+BurgerConstructor.propTypes = {
+  ingredients: PropTypes.array.isRequired,
+};
+
+export default BurgerConstructor;
