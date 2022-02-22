cc395573a04e17dad9978744477c68fdae43addd
Oleg Zharov <levineye@gmail.com>
2022-02-22 12:36:09 +0300

    feat: прокидывать пропсы от app

--- src/components/app/app.tsx
+++ src/components/app/app.tsx
@@ -3,13 +3,14 @@ import React from 'react';
 import style from './app.module.css';
 import AppHeader from '../app-header/app-header';
 import BurgerIngredients from '../burger-ingredients/burger-ingredients';
+import data from '../../utils/data';
 
 function App() {
   return (
     <div className={style.page}>
       <AppHeader />
       <main>
-        <BurgerIngredients />
+        <BurgerIngredients ingredients={data} />
       </main>
     </div>
   );
