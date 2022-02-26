a5b6c59d73a5cde66c415ec03d2b9d36d5b7995c
Oleg Zharov <levineye@gmail.com>
2022-02-26 20:04:32 +0300

    feat: сделать компонент создание бургера функциональным

--- src/components/burger-constructor/burger-constructor.tsx
+++ src/components/burger-constructor/burger-constructor.tsx
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React, { useState, useEffect } from 'react';
 import PropTypes from 'prop-types';
 import {
   ConstructorElement,
@@ -8,93 +8,66 @@ import {
 } from '@ya.praktikum/react-developer-burger-ui-components';
 
 import styles from './burger-constructor.module.css';
+import { sumByKey } from '../../utils/utils';
 
-class BurgerConstructor extends Component {
-  constructor(props) {
-    super(props);
+function BurgerConstructor({ bun = {}, ingredients = [] }) {
+  const [sum, setSum] = useState(0);
 
-    this.state = {
-      sum: 0,
-    };
-  }
+  useEffect(() => {
+    const sum = calculatePrice(ingredients, bun);
+    setSum(sum);
+  }, [bun, JSON.stringify(ingredients)]);
 
-  calculatePrice(arr, bun) {
-    const bunPrice = bun.price * 2 || 0;
+  const calculatePrice = (arr, bun) =>
+    sumByKey(arr, 'price') + (bun.price * 2 || 0);
 
-    return arr.reduce((acc, item) => {
-      return acc + item.price;
-    }, bunPrice);
-  }
-
-  setSum(ingredients, bun) {
-    const sum = this.calculatePrice(ingredients, bun);
-    this.setState({ sum });
-  }
-
-  componentDidMount() {
-    const { bun = {}, ingredients = [] } = this.props;
-    this.setSum(ingredients, bun);
-  }
-
-  componentDidUpdate(prevProps, prevState) {
-    const { bun, ingredients } = this.props;
-
-    if (bun !== prevProps.bun) {
-      this.setSum(ingredients, bun);
-    }
-  }
-
-  render() {
-    const { bun = {}, ingredients = [] } = this.props;
-
-    return (
-      <section className={`${styles.section} pt-25 ml-10 pl-4`}>
-        <ul className={styles.list}>
-          <li className={`${styles.item} mr-4`}>
-            <ConstructorElement
-              type="top"
-              thumbnail={bun.image}
-              text={`${bun.name} (верх)`}
-              price={bun.price}
-              isLocked={true}
-            />
-          </li>
-          <ul className={styles.sublist}>
-            {ingredients.map((item, index) => (
-              <li key={item._id} className={`${styles.subitem} mr-2`}>
-                <DragIcon type="primary" />
-                <ConstructorElement
-                  type={undefined}
-                  thumbnail={item.image}
-                  text={item.name}
-                  price={item.price}
-                  isLocked={false}
-                />
-              </li>
-            ))}
-          </ul>
-          <li className={`${styles.item} mr-4`}>
-            <ConstructorElement
-              type="bottom"
-              thumbnail={bun.image}
-              text={`${bun.name} (низ)`}
-              price={bun.price}
-              isLocked={true}
-            />
-          </li>
+  return (
+    <section className={`${styles.section} pt-25 ml-10 pl-4`}>
+      <ul className={styles.list}>
+        <li className={`${styles.item} mr-4`}>
+          <ConstructorElement
+            type="top"
+            thumbnail={bun.image}
+            text={`${bun.name} (верх)`}
+            price={bun.price}
+            isLocked={true}
+          />
+        </li>
+        <ul className={styles.sublist}>
+          {ingredients.map((item, index) => (
+            <li key={item._id} className={`${styles.subitem} mr-2`}>
+              <DragIcon type="primary" />
+              <ConstructorElement
+                type={undefined}
+                thumbnail={item.image}
+                text={item.name}
+                price={item.price}
+                isLocked={false}
+              />
+            </li>
+          ))}
         </ul>
-        <div className={`${styles.sum} mt-10`}>
-          <span className="text text_type_main-large mr-10">
-            <span className="mr-2">{this.state.sum}</span>
-            <CurrencyIcon type="primary" />
-          </span>
-          <Button type="primary" size="large" htmlType="submit">
-            Оформить заказ
-          </Button>
-        </div>
-      </section>
-    );
-  }
+        <li className={`${styles.item} mr-4`}>
+          <ConstructorElement
+            type="bottom"
+            thumbnail={bun.image}
+            text={`${bun.name} (низ)`}
+            price={bun.price}
+            isLocked={true}
+          />
+        </li>
+      </ul>
+      <div className={`${styles.sum} mt-10`}>
+        <span className="text text_type_main-large mr-10">
+          <span className="mr-2">{sum}</span>
+          <CurrencyIcon type="primary" />
+        </span>
+        <Button type="primary" size="large" htmlType="submit">
+          Оформить заказ
+        </Button>
+      </div>
+    </section>
+  );
 }
 
 BurgerConstructor.propTypes = {
