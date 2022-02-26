6b92b1c505b8c89afc0b06c04d56cb4bc3b99902
Oleg Zharov <levineye@gmail.com>
2022-02-26 21:25:46 +0300

    feat: добавит хук для получения данных из апи

---
+++ src/hooks/useFetch.tsx
@@ -0,0 +1,44 @@
+import React, { useState, useEffect } from 'react';
+
+export function useFetch(url, { method = 'GET', headers = {} } = {}) {
+  const [data, setData] = useState(null);
+  const [error, setError] = useState(null);
+  const [isSuccess, setIsSuccess] = useState(false);
+
+  useEffect(() => {
+    async function fetchData() {
+      try {
+        const res = await fetch(url, { method, headers });
+
+        if (!res.ok) {
+          throw res;
+        }
+
+        const { data } = await res.json();
+
+        setData(data);
+        setIsSuccess(true);
+      } catch (e) {
+        if (e instanceof Response) {
+          setError({
+            statusCode: e.status,
+            statusText: e.statusText,
+            headers: e.headers,
+          });
+        } else {
+          setError(e);
+        }
+
+        setIsSuccess(false);
+      }
+    }
+
+    fetchData();
+  }, [url]);
+
+  return {
+    data,
+    error,
+    success: isSuccess,
+  };
+}
