c2d66cf2782daa71af9d55e444364e79ae35f067
Oleg Zharov <levineye@gmail.com>
2022-02-27 20:52:25 +0300

    feat: подключить модалки, создать функции обработчики кликов

--- src/components/app/app.tsx
+++ src/components/app/app.tsx
@@ -1,11 +1,15 @@
 import React, { useState, useEffect } from 'react';
 
 import styles from './app.module.css';
+import { useFetch } from '../../hooks/useFetch';
 import AppHeader from '../app-header/app-header';
 import BurgerIngredients from '../burger-ingredients/burger-ingredients';
 import BurgerConstructor from '../burger-constructor/burger-constructor';
-import { useFetch } from '../../hooks/useFetch';
-import { API_URL } from '../../utils/constants';
+import Modal from '../modal/modal';
+import ModalOverlay from '../modal-overlay/modal-overlay';
+import IngredientDetails from '../ingredient-details/ingredient-details';
+import OrderDetails from '../order-details/order-details';
+import { API_URL, ESC_CODE } from '../../utils/constants';
 
 function App() {
   const [ingredients, setIngredients] = useState({
@@ -13,6 +17,10 @@ function App() {
     sauce: [],
     main: [],
   });
+  const [isOpenIngredientModal, setIsOpenIngredientModal] = useState(false);
+  const [isOpenOrderModal, setIsOpenOrderModal] = useState(false);
+  const [isOpenOverlay, setIsOpenOverlay] = useState(false);
+  const [currentIngredient, setCurrentIngredient] = useState(null);
 
   const { data, success } = useFetch(API_URL);
 
@@ -22,6 +30,18 @@ function App() {
       setIngredients(filteredIngredients);
     }
   }, [success, JSON.stringify(data)]);
+
+  useEffect(() => {
+    function handleClickEsc(e) {
+      if (e.keyCode === ESC_CODE) {
+        closeModals();
+      }
+    }
+
+    document.addEventListener('keydown', handleClickEsc);
+
+    return () => document.removeEventListener('keydown', handleClickEsc);
+  }, []);
 
   function filterIngredients(arr) {
     const ingredients = { bun: [], sauce: [], main: [] };
@@ -33,16 +53,49 @@ function App() {
     return ingredients;
   }
 
+  function closeModals() {
+    setIsOpenIngredientModal(false);
+    setIsOpenOrderModal(false);
+    setIsOpenOverlay(false);
+  }
+
+  function handleIngredientClick(ingredient) {
+    setCurrentIngredient(ingredient);
+    setIsOpenIngredientModal(true);
+    setIsOpenOverlay(true);
+  }
+
+  function handleOrderClick() {
+    setIsOpenOrderModal(true);
+    setIsOpenOverlay(true);
+  }
+
   return (
     <div className={styles.page}>
       <AppHeader />
       <main className={`${styles.main} pb-10`}>
-        <BurgerIngredients ingredients={ingredients} />
+        <BurgerIngredients
+          ingredients={ingredients}
+          onIngredientClick={handleIngredientClick}
+          onClose={closeModals}
+        />
         <BurgerConstructor
           bun={ingredients.bun[0]}
           ingredients={[...ingredients.sauce, ...ingredients.main]}
+          onButtonClick={handleOrderClick}
         />
       </main>
+      <ModalOverlay isOpen={isOpenOverlay} onClose={closeModals} />
+      <Modal
+        isOpen={isOpenIngredientModal}
+        onClose={closeModals}
+        title="Детали ингредиента"
+      >
+        <IngredientDetails {...currentIngredient} />
+      </Modal>
+      <Modal isOpen={isOpenOrderModal} onClose={closeModals}>
+        <OrderDetails />
+      </Modal>
     </div>
   );
 }
