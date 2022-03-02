1517bc7d89f72dadd36466aa595eac560dbedafc
Oleg Zharov <levineye@gmail.com>
2022-03-02 21:37:55 +0300

    feat: добавить константы для api и http

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -1,4 +1,12 @@
-export const API_URL = 'https://norma.nomoreparties.space/api/ingredients';
+export const API_BASE_URL = 'https://norma.nomoreparties.space/api';
+export const API_ENDPOINT = {
+  ingredients: '/ingredients',
+  orders: '/orders',
+};
+export const HTTP_METHOD = {
+  get: 'GET',
+  post: 'POST',
+};
 export const modalContainer = document.getElementById('modal');
 export const ESC_CODE = 27;
 
