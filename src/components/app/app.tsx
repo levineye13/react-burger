c0684c4609459833ec990a45a4645c230a01347d
Oleg Zharov <levineye@gmail.com>
2022-03-02 14:56:48 +0300

    feat: создать редьюсер для вычисления суммы заказа и обернуть компоненты в провайдер

--- src/components/app/app.tsx
+++ src/components/app/app.tsx
@@ -1,7 +1,8 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useReducer } from 'react';
 
 import styles from './app.module.css';
 import { useFetch } from '../../hooks/useFetch';
+import { IngredientsContext } from '../../context/IngredientsContext';
 import AppHeader from '../app-header/app-header';
 import BurgerIngredients from '../burger-ingredients/burger-ingredients';
 import BurgerConstructor from '../burger-constructor/burger-constructor';
@@ -10,7 +11,20 @@ import IngredientDetails from '../ingredient-details/ingredient-details';
 import OrderDetails from '../order-details/order-details';
 import { API_URL } from '../../utils/constants';
 
+const initialState = { sum: 0 };
+
+function reducer(state, action) {
+  switch (action.type) {
+    case 'setSum':
+      return { ...state, sum: action.payload };
+    default:
+      throw new Error(`Wrong type of action: ${action.type}`);
+  }
+}
+
 function App() {
+  const [sumState, sumDispatcher] = useReducer(reducer, initialState);
+
   const [ingredients, setIngredients] = useState({
     bun: [],
     sauce: [],
@@ -19,6 +33,7 @@ function App() {
   const [isOpenIngredientModal, setIsOpenIngredientModal] = useState(false);
   const [isOpenOrderModal, setIsOpenOrderModal] = useState(false);
   const [currentIngredient, setCurrentIngredient] = useState(null);
+  const [currentBun, setCurrentBun] = useState({});
 
   const { data, success } = useFetch(API_URL);
 
@@ -56,18 +71,22 @@ function App() {
   return (
     <div className={styles.page}>
       <AppHeader />
-      <main className={`${styles.main} pb-10`}>
-        <BurgerIngredients
-          ingredients={ingredients}
-          onIngredientClick={handleIngredientClick}
-          onClose={closeModals}
-        />
-        <BurgerConstructor
-          bun={ingredients.bun[0]}
-          ingredients={[...ingredients.sauce, ...ingredients.main]}
-          onButtonClick={handleOrderClick}
-        />
-      </main>
+      <IngredientsContext.Provider
+        value={{
+          ingredients,
+          currentBun: ingredients.bun[0] || {},
+          sum: sumState.sum,
+          sumDispatcher,
+        }}
+      >
+        <main className={`${styles.main} pb-10`}>
+          <BurgerIngredients
+            onIngredientClick={handleIngredientClick}
+            onClose={closeModals}
+          />
+          <BurgerConstructor onButtonClick={handleOrderClick} />
+        </main>
+      </IngredientsContext.Provider>
       {isOpenIngredientModal && (
         <Modal onClose={closeModals} title="Детали ингредиента">
           <IngredientDetails {...currentIngredient} />
