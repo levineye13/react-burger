db1e4bf90f7a0511da4825cc14848fbbc4e3d669
Oleg Zharov <levineye@gmail.com>
2022-03-02 21:40:10 +0300

    feat: в деталях заказа (модалке) отображать номер заказа

--- src/components/order-details/order-details.tsx
+++ src/components/order-details/order-details.tsx
@@ -3,10 +3,12 @@ import React from 'react';
 import style from './order-details.module.css';
 import OrderDone from '../../images/done.jpg';
 
-function OrderDetails() {
+function OrderDetails({ orderNumber }) {
   return (
     <article className={`${style.article} mt-15`}>
-      <h2 className={`${style.title} text text_type_digits-large`}>034536</h2>
+      <h2 className={`${style.title} text text_type_digits-large`}>
+        {orderNumber}
+      </h2>
       <p className="text text_type_main-medium mt-8">идентификатор заказа</p>
       <img className="mt-15" src={OrderDone} alt="Заказ готов" />
       <p className="text text_type_main-default mt-15">
