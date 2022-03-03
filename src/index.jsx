773549b1c721819cba0655fde0375988e38b38d2
Oleg Zharov <levineye@gmail.com>
2022-03-03 18:29:00 +0300

    fix: переименовать tsx в jsx пока нет типизации, чтобы не было ошибки типизации при запуске

--- src/index.jsx
+++ src/index.jsx
@@ -1,8 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+
 import './index.css';
 import App from './components/app/app';
-import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
   <React.StrictMode>
@@ -10,5 +10,3 @@ ReactDOM.render(
   </React.StrictMode>,
   document.getElementById('root')
 );
-
-reportWebVitals();
