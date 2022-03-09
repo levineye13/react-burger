9c6ba8f6a3a0fc83bc650fcbdd0fadcb8fdf8da3
Oleg Zharov <levineye@gmail.com>
2022-03-09 15:18:48 +0300

    feat: создать store, rootreducer, папки для редьюсеров и экшенов, подключить devtools

---
+++ src/services/store.js
@@ -0,0 +1,13 @@
+import { compose, createStore, applyMiddleware } from 'redux';
+import thunk from 'redux-thunk';
+
+import { rootReducer } from '../services/reducers';
+
+const composeEnhancers =
+  typeof window === 'object' && window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__
+    ? window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__({})
+    : compose;
+
+const enhancers = composeEnhancers(applyMiddleware(thunk));
+
+export const store = createStore(rootReducer, enhancers);
