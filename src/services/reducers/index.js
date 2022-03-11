0e45f30607ae0b33757c047bdf14f171f5c0424a
Oleg Zharov <levineye@gmail.com>
2022-03-11 18:54:53 +0300

    feat: объединить редьюсеры в рутредьюсер

--- src/services/reducers/index.js
+++ src/services/reducers/index.js
@@ -1,3 +1,13 @@
 import { combineReducers } from 'redux';
 
-export const rootReducer = combineReducers({});
+import { ingredientsReducer } from './burger-ingredients';
+import { burgerConstructorReducer } from './constructor-ingredients';
+import { currentIngredientReducer } from './current-ingredient';
+import { orderReducer } from './order';
+
+export const rootReducer = combineReducers({
+  ingredients: ingredientsReducer,
+  burgerConstructor: burgerConstructorReducer,
+  currentIngredient: currentIngredientReducer,
+  order: orderReducer,
+});
