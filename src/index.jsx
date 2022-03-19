c19ad6fcfadf5279abea357a230cc35f2166a767
Oleg Zharov <levineye@gmail.com>
2022-03-19 17:57:25 +0300

    feat: подключить реакт роутер

--- src/index.jsx
+++ src/index.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
+import { BrowserRouter as Router } from 'react-router-dom';
 
 import './index.css';
 import App from './components/app/app';
@@ -9,7 +10,9 @@ import { store } from './services/store';
 ReactDOM.render(
   <React.StrictMode>
     <Provider store={store}>
-      <App />
+      <Router>
+        <App />
+      </Router>
     </Provider>
   </React.StrictMode>,
   document.getElementById('root')
