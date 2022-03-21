1e58e9d70185fddf1a204b3a28254a0bb229514f
Oleg Zharov <levineye@gmail.com>
2022-03-21 17:29:51 +0300

    feat: добавить в форму стили

--- src/components/form/form.jsx
+++ src/components/form/form.jsx
@@ -1,9 +1,11 @@
 import React from 'react';
 
+import styles from './form.module.css';
+
 function Form({ children, name, onSubmit }) {
   return (
-    <form action="#" name={name} onSubmit={onSubmit}>
-      {children}
+    <form action="#" name={name} onSubmit={onSubmit} className={styles.form}>
+      <fieldset className={styles.fieldset}>{children}</fieldset>
     </form>
   );
 }
