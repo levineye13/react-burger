81d7f0a5c1d76fd8466e79d41e4f4b0e9198c756
Oleg Zharov <levineye@gmail.com>
2022-03-21 17:33:56 +0300

    feat: добивить стили, кнопки в профайл

--- src/pages/profile/profile.jsx
+++ src/pages/profile/profile.jsx
@@ -1,47 +1,50 @@
 import React from 'react';
 import { NavLink } from 'react-router-dom';
-import { Input } from '@ya.praktikum/react-developer-burger-ui-components';
+import {
+  Input,
+  Button,
+} from '@ya.praktikum/react-developer-burger-ui-components';
 
-import styles from './profile.css';
+import styles from './profile.module.css';
 import Form from '../../components/form/form';
 import { PAGES } from '../../utils/constants';
 
 const { profile } = PAGES;
 
+const setActiveLink = ({ isActive }) =>
+  `${styles.link} text text_type_main-medium ${
+    isActive ? styles.link_active : 'text_color_inactive'
+  }`;
+
 function Profile() {
   return (
-    <section>
-      <nav>
-        <ul>
-          <li>
-            <NavLink
-              to={profile}
-              className="text text_type_main-default text_color_inactive"
-              activeClassName={styles.link}
-            >
-              Профиль
-            </NavLink>
-          </li>
-          <li>
-            <NavLink
-              to="#"
-              className="text text_type_main-default text_color_inactive"
-              activeClassName={styles.link}
-            >
-              История заказов
-            </NavLink>
-          </li>
-          <li>
-            <NavLink
-              to="#"
-              className="text text_type_main-default text_color_inactive"
-              activeClassName={styles.link}
-            >
-              Выход
-            </NavLink>
-          </li>
-        </ul>
-      </nav>
+    <section className={`${styles.section} mt-30`}>
+      <div className={`${styles.menu} mr-15`}>
+        <nav>
+          <ul className={styles.list}>
+            <li className={styles.item}>
+              <NavLink to={profile} className={setActiveLink}>
+                Профиль
+              </NavLink>
+            </li>
+            <li className={styles.item}>
+              <NavLink to="/" className={setActiveLink}>
+                История заказов
+              </NavLink>
+            </li>
+            <li className={styles.item}>
+              <NavLink to="/" className={setActiveLink}>
+                Выход
+              </NavLink>
+            </li>
+          </ul>
+        </nav>
+        <p
+          className={`${styles.paragraph} text text_type_main-default text_color_inactive mt-20`}
+        >
+          В этом разделе вы можете изменить свои персональные данные
+        </p>
+      </div>
       <Form name="profile">
         <Input type="text" name="name" placeholder="Имя" icon="EditIcon" />
         <Input type="email" name="email" placeholder="Логин" icon="EditIcon" />
@@ -51,6 +54,14 @@ function Profile() {
           placeholder="Пароль"
           icon="EditIcon"
         />
+        <div className={styles.buttons}>
+          <Button type="secondary" size="medium" htmlType="button">
+            Отмена
+          </Button>
+          <Button type="primary" size="medium" htmlType="submit">
+            Сохранить
+          </Button>
+        </div>
       </Form>
     </section>
   );
