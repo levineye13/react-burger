d3e144901ed85c780e38c17aa6ad6b22a15dfdf7
Oleg Zharov <levineye@gmail.com>
2022-03-21 17:31:30 +0300

    feat: добивить стили для секции аутентификации

--- src/components/authentication-section/authentication-section.jsx
+++ src/components/authentication-section/authentication-section.jsx
@@ -1,9 +1,11 @@
 import React from 'react';
 
+import styles from './authentication-section.module.css';
+
 function AuthenticationSection({ children, title }) {
   return (
-    <section>
-      <h2>{title}</h2>
+    <section className={styles.section}>
+      <h2 className="text text_type_main-medium mb-6">{title}</h2>
       {children}
     </section>
   );
