0d55cc69e100859c814e046c75db8fb6df956995
Oleg Zharov <levineye@gmail.com>
2022-03-29 18:29:17 +0300

    feat: дополнить форму

--- src/components/form/form.jsx
+++ src/components/form/form.jsx
@@ -1,13 +1,33 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
 import styles from './form.module.css';
 
 function Form({ children, name, onSubmit }) {
+  function handleSubmit(e) {
+    e.preventDefault();
+    onSubmit();
+  }
+
   return (
-    <form action="#" name={name} onSubmit={onSubmit} className={styles.form}>
+    <form
+      action="#"
+      name={name}
+      onSubmit={handleSubmit}
+      className={styles.form}
+    >
       <fieldset className={styles.fieldset}>{children}</fieldset>
     </form>
   );
 }
+
+Form.propTypes = {
+  children: PropTypes.oneOfType([
+    PropTypes.arrayOf(PropTypes.element),
+    PropTypes.element,
+  ]).isRequired,
+  name: PropTypes.string.isRequired,
+  onSubmit: PropTypes.func.isRequired,
+};
 
 export default Form;
