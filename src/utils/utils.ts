4bf7ad6f1bd3dc896bf9ef72bc44295465dbe1cb
Oleg Zharov <levineye@gmail.com>
2022-04-11 18:44:49 +0300

    feat: добавить функцию удаления из токена его типа

--- src/utils/utils.ts
+++ src/utils/utils.ts
@@ -1,3 +1,11 @@
 export function sumByKey(arr: Array<any>, property: string): number {
   return arr.reduce((sum, item) => sum + (item[property] || 0), 0);
 }
+
+export function extractToken(
+  obj: { [key: string]: any },
+  field: string,
+  schemaType: string
+): void {
+  obj[field] = obj[field].replace(`${schemaType} `, '');
+}
