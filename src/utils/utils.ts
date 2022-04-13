09a6e01e8b6867866a6ee32b61e4b7838beadb5d
Oleg Zharov <levineye@gmail.com>
2022-04-13 17:43:20 +0300

    feat: типизировать бургер конструктор

--- src/utils/utils.ts
+++ src/utils/utils.ts
@@ -1,5 +1,5 @@
 export function sumByKey(arr: Array<any>, property: string): number {
-  return arr.reduce((sum, item) => sum + (item[property] || 0), 0);
+  return arr.reduce((sum: number, item: any) => sum + (item[property] || 0), 0);
 }
 
 export function extractToken(
