de7d88f065e4c656b16701dd98d5a1d0964147b4
Oleg Zharov <levineye@gmail.com>
2022-04-18 11:08:39 +0300

    feat: типизировать защищенный роут при помощи RouteProps

--- src/hoc/protected-route.tsx
+++ src/hoc/protected-route.tsx
@@ -1,16 +1,16 @@
 import React, { FC, ReactElement } from 'react';
 import { Route, Redirect } from 'react-router-dom';
 import { useSelector } from 'react-redux';
+import { RouteProps } from 'react-router-dom';
 
 import { PAGES } from '../utils/constants';
 
 const { login } = PAGES;
 
-interface IProps {
-  [prop: string]: any;
-}
-
-const ProtectedRoute: FC<IProps> = ({ children, ...props }): ReactElement => {
+const ProtectedRoute: FC<RouteProps> = ({
+  children,
+  ...props
+}): ReactElement => {
   const { isAuth } = useSelector((state: any) => state.user);
 
   return (
