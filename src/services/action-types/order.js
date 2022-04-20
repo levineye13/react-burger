4ef44a11fb1769b47f0ce8d6c8a0a627eeb36527
Oleg Zharov <levineye@gmail.com>
2022-04-20 16:39:22 +0300

    feat: переписать типы экшенов на ts

--- src/services/action-types/order.js
+++
@@ -1,3 +0,0 @@
-export const OPEN_ORDER = 'OPEN_ORDER';
-export const CLOSE_ORDER = 'CLOSE_ORDER';
-export const MAKE_ORDER = 'MAKE_ORDER';
---
+++ src/services/action-types/order.ts
@@ -0,0 +1,3 @@
+export const OPEN_ORDER: 'OPEN_ORDER' = 'OPEN_ORDER';
+export const CLOSE_ORDER: 'CLOSE_ORDER' = 'CLOSE_ORDER';
+export const MAKE_ORDER: 'MAKE_ORDER' = 'MAKE_ORDER';
