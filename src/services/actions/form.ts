8397212a334c6327da1108c83fd568c89109796c
Oleg Zharov <levineye@gmail.com>
2022-04-23 17:00:39 +0300

    feat: типизировать экшены формы

---
+++ src/services/actions/form.ts
@@ -0,0 +1,31 @@
+import { SET_FIELD_VALUE, CLEAR_FORM } from '../action-types';
+
+// ======= Actions =======
+
+export interface ISetFieldValue {
+  readonly type: typeof SET_FIELD_VALUE;
+  readonly payload: { formName: string; field: string; value: string | number };
+}
+
+export interface IClearForm {
+  readonly type: typeof CLEAR_FORM;
+  readonly payload: { formName: string };
+}
+
+export type TForm = ISetFieldValue | IClearForm;
+
+// ======= Action Creators =======
+
+export const setFieldValue = (payload: {
+  formName: string;
+  field: string;
+  value: string | number;
+}): ISetFieldValue => ({
+  type: SET_FIELD_VALUE,
+  payload,
+});
+
+export const clearForm = (formName: string): IClearForm => ({
+  type: CLEAR_FORM,
+  payload: { formName },
+});
