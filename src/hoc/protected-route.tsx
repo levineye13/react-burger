caf550f8d215be4a32280e41e9d656c0dbfc9420
Oleg Zharov <levineye@gmail.com>
2022-04-25 17:41:17 +0300

    feat: типизировать редакс компонента защищенного роута

--- src/hoc/protected-route.tsx
+++ src/hoc/protected-route.tsx
@@ -1,15 +1,15 @@
 import React, { FC, ReactElement } from 'react';
 import { Route, Redirect } from 'react-router-dom';
-import { useSelector } from 'react-redux';
 import { RouteProps } from 'react-router-dom';
 
+import { useSelector } from '../hooks/useSelector';
 import { Pages } from '../utils/constants';
 
 const ProtectedRoute: FC<RouteProps> = ({
   children,
   ...props
 }): ReactElement => {
-  const { isAuth } = useSelector((state: any) => state.user);
+  const { isAuth } = useSelector((state) => state.user);
 
   return (
     <Route
