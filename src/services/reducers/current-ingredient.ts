85df8416dc9a14e24aaf218a4e52447dbadf6182
Oleg Zharov <levineye@gmail.com>
2022-04-25 13:56:51 +0300

    feat: типизировать редьюсер текущего ингредиента

---
+++ src/services/reducers/current-ingredient.ts
@@ -0,0 +1,20 @@
+import { IIngredient } from '../../utils/interfaces';
+import { SET_INGREDIENT } from '../action-types';
+import { TCurrentIngredient } from '../actions/current-ingredient';
+
+type CurrentIngredientState = IIngredient | null;
+
+const initialCurrentIngredient: CurrentIngredientState = null;
+
+export const currentIngredientReducer = (
+  state = initialCurrentIngredient,
+  action: TCurrentIngredient
+): CurrentIngredientState => {
+  switch (action.type) {
+    case SET_INGREDIENT:
+      return { ...action.payload };
+
+    default:
+      return state;
+  }
+};
