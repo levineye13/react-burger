c2848c809bcec5fc084c569b80ef278593a8b805
Oleg Zharov <levineye@gmail.com>
2022-05-08 21:13:26 +0300

    feat: убрать хардкод из статистики

--- src/components/statistic/statistic.tsx
+++ src/components/statistic/statistic.tsx
@@ -1,64 +1,71 @@
-import React, { FC, ReactElement } from 'react';
+import React, { FC, ReactElement, useMemo } from 'react';
 
 import styles from './statistic.module.css';
+import { useSelector } from '../../hooks';
+import { TOrderResponce } from '../../utils/types';
+import { OrderStatus } from '../../utils/constants';
+
+type TSortedOrders = {
+  ready: ReadonlyArray<TOrderResponce>;
+  notReady: ReadonlyArray<TOrderResponce>;
+};
 
 const Statistic: FC = (): ReactElement => {
+  const { total, totalToday, allOrders } = useSelector(
+    (state) => state.webSocket
+  );
+
+  const orders: TSortedOrders = useMemo(
+    () =>
+      allOrders.reduce(
+        (acc: TSortedOrders, order: TOrderResponce) => {
+          if (order.status === OrderStatus.Done) {
+            return { ...acc, ready: [...acc.ready, order] };
+          }
+
+          return { ...acc, notReady: [...acc.notReady, order] };
+        },
+        { ready: [], notReady: [] }
+      ),
+    [allOrders]
+  );
+
   return (
     <section className={styles.section}>
       <ul className={styles.list}>
         <li className="item item__title mb-6">
           <p className="text text_type_main-medium">Готовы:</p>
         </li>
-        <li className="item mb-2">
-          <p className={`${styles.success} text text_type_digits-default`}>
-            034533
-          </p>
-        </li>
-        <li className="item mb-2">
-          <p className={`${styles.success} text text_type_digits-default`}>
-            034533
-          </p>
-        </li>
-        <li className="item mb-2">
-          <p className={`${styles.success} text text_type_digits-default`}>
-            034533
-          </p>
-        </li>
-        <li className="item mb-2">
-          <p className={`${styles.success} text text_type_digits-default`}>
-            034533
-          </p>
-        </li>
-        <li className="item">
-          <p className={`${styles.success} text text_type_digits-default`}>
-            034533
-          </p>
-        </li>
+        {orders.ready.map((order) => (
+          <li className="item mb-2" key={order._id}>
+            <p className={`${styles.success} text text_type_digits-default`}>
+              {order.number}
+            </p>
+          </li>
+        ))}
       </ul>
       <ul className={styles.list}>
         <li className="item item__title mb-6">
           <p className="text text_type_main-medium">В работе:</p>
         </li>
-        <li className="item mb-2">
-          <p className="text text_type_digits-default">034538</p>
-        </li>
-        <li className="item mb-2">
-          <p className="text text_type_digits-default">034538</p>
-        </li>
-        <li className="item">
-          <p className="text text_type_digits-default">034538</p>
-        </li>
+        {orders.notReady.map((order) => (
+          <li className="item" key={order._id}>
+            <p className="text text_type_digits-default">{order.number}</p>
+          </li>
+        ))}
       </ul>
       <p className={`${styles.statistic} text text_type_main-medium mt-15`}>
         Выполнено за все время:
       </p>
       <span className={`${styles.digit} text text_type_digits-large`}>
-        28 752
+        {total}
       </span>
       <p className={`${styles.statistic} text text_type_main-medium mt-15`}>
         Выполнено за сегодня:
       </p>
-      <span className={`${styles.digit} text text_type_digits-large`}>138</span>
+      <span className={`${styles.digit} text text_type_digits-large`}>
+        {totalToday}
+      </span>
     </section>
   );
 };
