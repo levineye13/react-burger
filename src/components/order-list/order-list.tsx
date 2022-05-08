e75c60fa3ca99aaaeba096b4c7781b48f658a398
Oleg Zharov <levineye@gmail.com>
2022-05-08 21:14:32 +0300

    feat: убрать хардкод из списка заказов

--- src/components/order-list/order-list.tsx
+++ src/components/order-list/order-list.tsx
@@ -1,32 +1,43 @@
 import React, { FC, ReactElement } from 'react';
 
 import styles from './order-list.module.css';
-import Order from '../order/order';
+import OrderItem from '../order-item/order-item';
 import { useSelector } from '../../hooks';
 
 interface IProps {
-  maxWidth?: number | string;
+  readonly maxWidth?: number | string;
 }
 
 const OrderList: FC<IProps> = ({ maxWidth = 'none' }): ReactElement => {
+  const { allOrders } = useSelector((state) => state.webSocket);
+
   return (
     <section className={`${styles.section} pl-2`} style={{ maxWidth }}>
       <ul className={styles.list}>
-        <li className={styles.item}>
-          <Order />
-        </li>
-        <li className={styles.item}>
-          <Order />
-        </li>
-        <li className={styles.item}>
-          <Order />
-        </li>
-        <li className={styles.item}>
-          <Order />
-        </li>
-        <li className={styles.item}>
-          <Order />
-        </li>
+        {allOrders.map((order) => {
+          const timestamp = new Date(order.createdAt).toLocaleDateString(
+            'ru-RU',
+            {
+              formatMatcher: 'best fit',
+              weekday: 'long',
+              hour: '2-digit',
+              minute: '2-digit',
+              timeZoneName: 'short',
+            }
+          );
+
+          return (
+            <li className={styles.item} key={order._id}>
+              <OrderItem
+                number={order.number}
+                title={order.name}
+                timestamp={timestamp}
+                status={order.status}
+                ingredients={order.ingredients}
+              />
+            </li>
+          );
+        })}
       </ul>
     </section>
   );
