7349c2b5437d4e15d820736a85c2d774b5611627
Oleg Zharov <levineye@gmail.com>
2022-05-09 15:56:19 +0300

    feat: сортировать заказы и разбивать на подмассивы из 10 элементов

--- src/components/statistic/statistic.tsx
+++ src/components/statistic/statistic.tsx
@@ -6,8 +6,8 @@ import { TOrderResponce } from '../../utils/types';
 import { OrderStatus } from '../../utils/constants';
 
 type TSortedOrders = {
-  ready: ReadonlyArray<TOrderResponce>;
-  notReady: ReadonlyArray<TOrderResponce>;
+  ready: Array<Array<TOrderResponce>>;
+  notReady: Array<Array<TOrderResponce>>;
 };
 
 const Statistic: FC = (): ReactElement => {
@@ -15,42 +15,79 @@ const Statistic: FC = (): ReactElement => {
     (state) => state.webSocket
   );
 
-  const orders: TSortedOrders = useMemo(
-    () =>
-      allOrders.reduce(
-        (acc: TSortedOrders, order: TOrderResponce) => {
-          if (order.status === OrderStatus.Done) {
-            return { ...acc, ready: [...acc.ready, order] };
-          }
+  const orders: TSortedOrders = useMemo(() => {
+    const res: TSortedOrders = { ready: [], notReady: [] };
+    let lastReadyArr: Array<TOrderResponce> = [];
+    let lastNotReadyArr: Array<TOrderResponce> = [];
 
-          return { ...acc, notReady: [...acc.notReady, order] };
-        },
-        { ready: [], notReady: [] }
-      ),
-    [allOrders]
-  );
+    for (let index = 0; index < allOrders.length; index++) {
+      const current = allOrders[index];
+
+      if (current.status === OrderStatus.Done) {
+        if (lastReadyArr.length >= 10) {
+          res.ready.push([...lastReadyArr]);
+          lastReadyArr.length = 0;
+        } else {
+          lastReadyArr.push(current);
+        }
+      } else {
+        if (lastNotReadyArr.length >= 10) {
+          res.notReady.push([...lastNotReadyArr]);
+          lastNotReadyArr.length = 0;
+        } else {
+          lastNotReadyArr.push(current);
+        }
+      }
+    }
+
+    if (lastReadyArr.length > 0) {
+      res.ready.push([...lastReadyArr]);
+    }
+
+    if (lastNotReadyArr.length > 0) {
+      res.notReady.push([...lastNotReadyArr]);
+    }
+
+    return res;
+  }, [allOrders]);
 
   return (
     <section className={styles.section}>
       <ul className={styles.list}>
-        <li className="item item__title mb-6">
+        <li className={`${styles.capture} item__title mb-6`}>
           <p className="text text_type_main-medium">Готовы:</p>
         </li>
-        {orders.ready.map((order) => (
-          <li className="item mb-2" key={order._id}>
-            <p className={`${styles.success} text text_type_digits-default`}>
-              {order.number}
-            </p>
+        {orders.ready.map((arr, index) => (
+          <li className={`${styles.item} mr-3 mb-6`} key={index}>
+            <ul className={styles.sublist}>
+              {arr.map((order) => (
+                <li className={`${styles.subitem} mb-2`} key={order._id}>
+                  <p
+                    className={`${styles.success} text text_type_digits-default`}
+                  >
+                    {order.number}
+                  </p>
+                </li>
+              ))}
+            </ul>
           </li>
         ))}
       </ul>
       <ul className={styles.list}>
-        <li className="item item__title mb-6">
+        <li className={`${styles.capture} item__title mb-6`}>
           <p className="text text_type_main-medium">В работе:</p>
         </li>
-        {orders.notReady.map((order) => (
-          <li className="item" key={order._id}>
-            <p className="text text_type_digits-default">{order.number}</p>
+        {orders.notReady.map((arr, index) => (
+          <li className={`${styles.item} mr-3 mb-6`} key={index}>
+            <ul className={styles.sublist}>
+              {arr.map((order) => (
+                <li className="item" key={order._id}>
+                  <p className="text text_type_digits-default">
+                    {order.number}
+                  </p>
+                </li>
+              ))}
+            </ul>
           </li>
         ))}
       </ul>
