f0b233ac77c73521012ead37b45aee5e61bd6f55
Oleg Zharov <levineye@gmail.com>
2022-05-11 18:42:38 +0300

    feat: написать функцию получения статуса заказа

--- src/utils/utils.ts
+++ src/utils/utils.ts
@@ -1,17 +1,18 @@
-export function sumByKey(
+import { OrderStatus } from './constants';
+export const sumByKey = (
   arr: ReadonlyArray<any> | Array<any>,
   property: string
-): number {
+): number => {
   return arr.reduce((sum: number, item: any) => sum + (item[property] || 0), 0);
-}
+};
 
-export function extractToken(
+export const extractToken = (
   obj: { [key: string]: any },
   field: string,
   schemaType: string
-): void {
+): void => {
   obj[field] = obj[field].replace(`${schemaType} `, '');
-}
+};
 
 export const dateFormat = (date: string): string =>
   new Date(date).toLocaleDateString('ru-RU', {
@@ -21,3 +22,19 @@ export const dateFormat = (date: string): string =>
     minute: '2-digit',
     timeZoneName: 'short',
   });
+
+export const getStatusOrder = (status: string, locale: 'Ru' | 'En'): string => {
+  switch (status) {
+    case OrderStatus.Done.En:
+      return OrderStatus.Done[locale];
+
+    case OrderStatus.Pending.En:
+      return OrderStatus.Pending[locale];
+
+    case OrderStatus.Created.En:
+      return OrderStatus.Created[locale];
+
+    default:
+      return '';
+  }
+};
