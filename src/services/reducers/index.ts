1a88d1ab93274b401036bb7b5382ab3fc6350b4e
Oleg Zharov <levineye@gmail.com>
2022-05-17 13:00:04 +0300

    fix: поправить пути

--- src/services/reducers/index.ts
+++ src/services/reducers/index.ts
@@ -1,12 +1,12 @@
 import { combineReducers } from 'redux';
 
-import { userReducer } from './user';
-import { ingredientsReducer } from './burger-ingredients';
-import { burgerConstructorReducer } from './constructor-ingredients';
-import { currentIngredientReducer } from './current-ingredient';
-import { orderReducer } from './order';
-import { formReducer } from './form';
-import { webSocketReducer } from './web-socket';
+import { userReducer } from './user/user';
+import { ingredientsReducer } from './burger-ingredients/burger-ingredients';
+import { burgerConstructorReducer } from './constructor-ingredients/constructor-ingredients';
+import { currentIngredientReducer } from './current-ingredient/current-ingredient';
+import { orderReducer } from './order/order';
+import { formReducer } from './form/form';
+import { webSocketReducer } from './web-socket/web-socket';
 
 export const rootReducer = combineReducers({
   user: userReducer,
